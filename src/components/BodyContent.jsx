6547dffc0fe873d1e274a9c201de42ce4316db34
Anirudh Panwar <anirudhpanwar7777@gmail.com>
2025-04-20 03:31:11 +0530

    Fix maxW layout of BodyContent

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -2,15 +2,17 @@ import { Flex } from "@chakra-ui/react"
 
 const BodyContent = () => {
     return (
-        <Flex
-            flexDirection={'column'}
-            height={'100vh'}
-            border={'2px solid blue'}
-            maxWidth={'600px'}
-            minWidth={'200px'}
-            mx={{base: '24px', md: '48px', lg: '64px'}}
-        >
+        <Flex width={'100%'} justifyContent={'center'}>
+            <Flex
+                width={'100%'}
+                flexDirection={'column'}
+                height={'100vh'}
+                border={'2px solid blue'}
+                maxWidth={'800px'}
+                mx={{ base: '24px', md: '48px', lg: '64px' }}
+            >
 
+            </Flex>
         </Flex>
     )
 }
