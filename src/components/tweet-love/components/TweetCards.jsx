fe41d5af908fd9359d9b921b8e0c25eb600ce293
Anirudh Panwar <anirudhpanwar7777@gmail.com>
2025-04-24 01:25:37 +0530

    Fix tweetcards component for mobile

--- src/components/tweet-love/components/TweetCards.jsx
+++ src/components/tweet-love/components/TweetCards.jsx
@@ -1,5 +1,6 @@
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex, Show } from "@chakra-ui/react"
 import TweetCard from "./TweetCard"
+import Marquee from "react-fast-marquee"
 
 const TweetCards = () => {
     return (
@@ -8,23 +9,44 @@ const TweetCards = () => {
             align={'center'}
             mt={'32px'}
             width={'80vw'}
+            maxWidth={'900%'}
+            justifyContent={'center'}
             overflow={'scroll'}
         >
-            <TweetCard
-                height={'429px'}
-                width={'378px'}
-                content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
-            />
-            <TweetCard 
-                height={'517px'} 
-                width={'456px'}
-                content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
-            />
-            <TweetCard 
-                height={'429px'} 
-                width={'378px'}
-                content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
-            />
+            <Show below="md">
+                <TweetCard
+                    height={'429px'}
+                    width={'378px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+                <TweetCard
+                    height={'429px'}
+                    width={'378px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+                <TweetCard
+                    height={'429px'}
+                    width={'378px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+            </Show>
+            <Show above="md">
+                <TweetCard
+                    height={'429px'}
+                    width={'378px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+                <TweetCard
+                    height={'517px'}
+                    width={'456px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+                <TweetCard
+                    height={'429px'}
+                    width={'378px'}
+                    content={"Companies spend $30,000+ and several weeks to build beautiful landing pages like@linear,@wopehq and lectnotes + I built@reactjs+@tailwindcss components for you to do the same in hours, starting at just $29 Pre-order link + demo below#buildinpublic@buildspace"}
+                />
+            </Show>
         </Flex>
     )
 }
